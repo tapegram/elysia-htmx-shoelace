f357b5d9da8ad533ca4e634d22a350733f807c1d
Tavish Pegram <tapegram@gmail.com>
2024-12-11 11:22:49 -0500

    Some script stuff for migrations

--- drizzle/0000_rare_redwing.sql
+++ drizzle/0000_rare_redwing.sql
@@ -1,11 +1,8 @@
 -- Current sql file was generated after introspecting the database
 -- If you want to run this migration please uncomment this code before executing migrations
-/*
-CREATE TABLE `users_table` (
+CREATE TABLE IF NOT EXISTS `users_table` (
 	`id` integer PRIMARY KEY AUTOINCREMENT NOT NULL,
 	`name` text NOT NULL,
 	`age` integer NOT NULL,
 	`email` text NOT NULL
 );
-
-*/
