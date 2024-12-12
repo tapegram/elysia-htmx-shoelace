63df53ea76db5ae5d8124d71ef78372ec6ea2f2c
Tavish Pegram <tapegram@gmail.com>
2024-12-12 11:42:29 -0500

    Local dev with local turso

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -4,8 +4,8 @@ import { drizzle } from 'drizzle-orm/libsql';
 const db = drizzle({
   connection: {
     url: process.env.TURSO_DATABASE_URL!,
-    authToken: process.env.TURSO_AUTH_TOKEN!
-  }
+    authToken: process.env.TURSO_AUTH_TOKEN,
+  },
 });
 
 export default db;
