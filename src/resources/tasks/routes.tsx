a0de002e2f8eb6ec849218d843efe37bbfc8be9e
Tavish Pegram <tapegram@gmail.com>
2024-12-12 22:59:48 -0500

    Adding a "tomorrow" button

--- src/resources/tasks/routes.tsx
+++ src/resources/tasks/routes.tsx
@@ -34,16 +34,24 @@ const TaskList = ({ tasks }: { tasks: Task[] }): JSX.Element => {
                 then remove @open from <sl-details/> in the closest <div/>
                 then add @open to the closest <sl-details />
                end
-               on keyup if the event's key is 'd' call alert('delete') end
+               on keyup if the event's key is 'x' call alert('delete') end
+               on keyup if the event's key is 't' call alert('tomorrow') end
                on keyup if the event's key is 'e' call alert('completed')"
             id={`task-${index}`}
-            class="w-2/3 max-w-md"
+            class="w-4/5"
             summary={task.title}
           >
             <p class="text-gray-700 mb-2">{task.description}</p>
-            <div class="flex gap-2 justify-end mt-2">
-              <sl-button tabindex={-1} variant="danger" onclick="alert('delete')">Delete</sl-button>
-              <sl-button tabindex={-1} variant="primary" onclick="alert('completed')">Done</sl-button>
+            <div class="flex gap-2 justify-end mt-4">
+              <sl-button tabindex={-1} variant="default" onclick="alert('delete')">
+                <sl-icon name="trash"></sl-icon>
+              </sl-button>
+              <sl-button tabindex={-1} variant="default" onclick="alert('tomorrow')">
+                <sl-icon name="clock"></sl-icon>
+              </sl-button>
+              <sl-button tabindex={-1} variant="default" onclick="alert('completed')">
+                <sl-icon name="check2"></sl-icon>
+              </sl-button>
             </div>
           </sl-details>
         </sl-animation>
