672a768f9e81563bf999a76f0b9a954e94f7ec7a
Tavish Pegram <tapegram@gmail.com>
2024-12-13 00:49:19 -0500

    fix textarea

--- src/resources/tasks/routes.tsx
+++ src/resources/tasks/routes.tsx
@@ -35,7 +35,7 @@ const TaskList = ({ tasks }: { tasks: Task[] }): JSX.Element => {
                   remove @open from <sl-details /> in closest <div />
                   then wait 5ms
                   add @play to the closest <sl-animation />
-                  then wait 100ms
+                  then wait 150ms
                   then add @open to me
                 end
                 on keyup if the event's key is 'x' call alert('delete') end
@@ -45,7 +45,7 @@ const TaskList = ({ tasks }: { tasks: Task[] }): JSX.Element => {
             class="w-4/5"
             summary={task.title}
           >
-            <sl-textarea _="on keyup halt the event" class="text-gray-700 mb-2" resize="auto" defaultValue={task.description}></sl-textarea>
+            <sl-textarea _="on keyup halt the event" class="text-gray-700 mb-2" resize="auto" value={task.description} />
             <div class="flex gap-2 justify-end mt-4">
               <sl-button tabindex={-1} variant="default" onclick="alert('delete')">
                 <sl-icon name="trash"></sl-icon>
