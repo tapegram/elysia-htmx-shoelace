c2ae41177773fa816e23dc51b93c9a8744d0c217
Tavish Pegram <tapegram@gmail.com>
2024-12-14 16:50:17 -0500

    use click events in key handlers

--- src/resources/tasks/routes.tsx
+++ src/resources/tasks/routes.tsx
@@ -70,22 +70,23 @@ const TaskList = ({ tasks }: { tasks: Task[] }): JSX.Element => {
                   then wait 150ms
                   then add @open to me
                 end
-                on keyup if the event's key is 'x' call alert('delete') end
-                on keyup if the event's key is 't' call alert('tomorrow') end
-                on keyup if the event's key is 'e' call alert('completed')`}
+                on keyup if the event's key is 'x' call #task-${index}-delete.click() end
+                on keyup if the event's key is 't' call #task-${index}-defer.click() end
+                on keyup if the event's key is 'e' call #task-${index}-complete.click() end
+            `}
             id={`task-${index}`}
             class="w-4/5"
             summary={task.title}
           >
             <sl-textarea _="on keyup halt the event" class="text-gray-700 mb-2" resize="auto" value={task.description} />
             <div class="flex gap-2 justify-end mt-4">
-              <sl-button tabindex={-1} variant="default" onclick="alert('delete')">
+              <sl-button tabindex={-1} id={`task-${index}-delete`} variant="default" onclick="alert('delete')">
                 <sl-icon name="trash"></sl-icon>
               </sl-button>
-              <sl-button tabindex={-1} variant="default" onclick="alert('tomorrow')">
+              <sl-button tabindex={-1} id={`task-${index}-defer`} variant="default" onclick="alert('tomorrow')">
                 <sl-icon name="clock"></sl-icon>
               </sl-button>
-              <sl-button tabindex={-1} variant="default" onclick="alert('completed')">
+              <sl-button tabindex={-1} id={`task-${index}-complete`} variant="default" onclick="alert('completed')">
                 <sl-icon name="check2"></sl-icon>
               </sl-button>
             </div>
