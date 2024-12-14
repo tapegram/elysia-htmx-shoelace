f32a6e6e63c186a2cb88a44b4d6973676a88043f
Tavish Pegram <tapegram@gmail.com>
2024-12-14 17:58:52 -0500

    Return new task

--- src/resources/tasks/routes.tsx
+++ src/resources/tasks/routes.tsx
@@ -9,6 +9,17 @@ export function addTasksRoutes(app: Elysia) {
     return redirect("/tasks")
   })
 
+  app.post("/tasks", ({ body }) => {
+    const newTask = {
+      id: 4,
+      title: body.summary,
+      completed: false,
+      description: body.description,
+    }
+
+    return <TaskItem task={newTask} />
+  });
+
   app.get("/tasks", (context: HtmxContext) => {
     const tasks = [
       { id: 1, title: 'Task 1', completed: false, description: 'This is a description for Task 1' },
@@ -75,37 +86,42 @@ const NewTaskDialog = (): JSX.Element => {
 const TaskList = ({ tasks }: { tasks: Task[] }): JSX.Element => {
   return (
     <div class="flex flex-col gap-4 items-center">
-      {tasks.map((task, index) => (
-        <sl-animation name="pulse" duration="500" iterations="1" easing="easeOutCubic">
-          <sl-details
-            _={`
-                on focus 
-                  add @play to the closest <sl-animation />
-                end
-                on keyup if the event's key is 'x' call #task-${index}-delete.click() end
-                on keyup if the event's key is 't' call #task-${index}-defer.click() end
-                on keyup if the event's key is 'e' call #task-${index}-complete.click() end
-            `}
-            id={`task-${index}`}
-            class="w-4/5"
-            summary={task.title}
-          >
-            <sl-textarea _="on keyup halt the event" class="text-gray-700 mb-2" resize="auto" value={task.description} />
-            <div class="flex gap-2 justify-end mt-4">
-              <sl-button tabindex={-1} id={`task-${index}-delete`} variant="default" onclick="alert('delete')">
-                <sl-icon name="trash"></sl-icon>
-              </sl-button>
-              <sl-button tabindex={-1} id={`task-${index}-defer`} variant="default" onclick="alert('tomorrow')">
-                <sl-icon name="clock"></sl-icon>
-              </sl-button>
-              <sl-button tabindex={-1} id={`task-${index}-complete`} variant="default" onclick="alert('completed')">
-                <sl-icon name="check2"></sl-icon>
-              </sl-button>
-            </div>
-          </sl-details>
-        </sl-animation>
+      {tasks.map((task) => (
+        TaskItem({ task })
       ))
       }
     </div >
   );
+}
+
+const TaskItem = ({ task }: { task: Task }): JSX.Element => {
+
+  return <sl-animation name="pulse" duration="500" iterations="1" easing="easeOutCubic">
+    <sl-details
+      _={`
+                on focus 
+                  add @play to the closest <sl-animation />
+                end
+                on keyup if the event's key is 'x' call #task-${task.id}-delete.click() end
+                on keyup if the event's key is 't' call #task-${task.id}-defer.click() end
+                on keyup if the event's key is 'e' call #task-${task.id}-complete.click() end
+            `}
+      id={`task-${task.id}`}
+      class="w-4/5"
+      summary={task.title}
+    >
+      <sl-textarea _="on keyup halt the event" class="text-gray-700 mb-2" resize="auto" value={task.description} />
+      <div class="flex gap-2 justify-end mt-4">
+        <sl-button tabindex={-1} id={`task-${task.id}-delete`} variant="default" onclick="alert('delete')">
+          <sl-icon name="trash"></sl-icon>
+        </sl-button>
+        <sl-button tabindex={-1} id={`task-${task.id}-defer`} variant="default" onclick="alert('tomorrow')">
+          <sl-icon name="clock"></sl-icon>
+        </sl-button>
+        <sl-button tabindex={-1} id={`task-${task.id}-complete`} variant="default" onclick="alert('completed')">
+          <sl-icon name="check2"></sl-icon>
+        </sl-button>
+      </div>
+    </sl-details>
+  </sl-animation>
 }
