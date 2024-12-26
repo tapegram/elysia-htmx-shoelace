0355be9f4fdbd5bfbc4926d8a876048017343f4b
Tavish Pegram <tapegram@gmail.com>
2024-12-26 13:07:32 -0500

    Small refactoring

--- src/main.ts
+++ src/main.ts
@@ -2,15 +2,12 @@ import { html, } from "@elysiajs/html";
 import open from "open";
 import staticPlugin from "@elysiajs/static";
 import { tailwind } from "@gtramontina.com/elysia-tailwind";
-import { Elysia, ListenCallback, redirect } from "elysia";
+import { Elysia, ListenCallback, } from "elysia";
 import { ElysiaWS } from "elysia/dist/ws";
 import addRoutes from "./routes";
 import { htmx } from "@gtramontina.com/elysia-htmx";
 import { getEnv } from "./shared";
 import { logger } from "@grotto/logysia";
-import { oauth2 } from "elysia-oauth2";
-import jwt from "@elysiajs/jwt";
-import { usersService } from "./resources/users/service";
 
 declare global {
   var ws: ElysiaWS<any, any, any>
@@ -19,64 +16,6 @@ declare global {
 
 export default function main() {
   const app = new Elysia()
-
-  app.use(
-    jwt({
-      name: 'boardsession',
-      secret: process.env.JWT_SECRET!,
-    })
-  )
-
-  app.use(
-    oauth2({
-      GitHub: [
-        process.env.GITHUB_CLIENT_ID!,
-        process.env.GITHUB_CLIENT_SECRET!,
-        process.env.FRONTEND_URL! + "/auth/github/callback",
-      ]
-    })
-  )
-    .get("/auth/github", async ({ oauth2 }) =>
-      oauth2.redirect("GitHub", [])
-    )
-    .get("/auth/github/callback", async ({ oauth2, boardsession, cookie: { auth } }) => {
-      const token = await oauth2.authorize("GitHub");
-      console.log(token);
-
-      console.log("before repsonse");
-      console.log(token.accessToken())
-      // Fetch user information from GitHub API
-      const response = await fetch("https://api.github.com/user", {
-        headers: {
-          'Authorization': `Bearer ${token.accessToken()}`,
-          'Accept': "application/vnd.github+json",
-          'X-GitHub-Api-Version': "2022-11-28",
-        },
-      });
-      console.log("response", response);
-
-      if (!response.ok) {
-        console.error("Failed to fetch user information from GitHub");
-        return;
-      }
-
-      const { id } = await response.json();
-      console.log("GitHub User ID:", id);
-
-      const user = await usersService.getOrCreateUser({ githubId: id });
-
-      console.log("User:", user);
-
-      auth.set({
-        value: await boardsession.sign({ id: user.id }),
-        httpOnly: true,
-        maxAge: 7 * 86400,
-      })
-
-      return redirect("/")
-    })
-
-
 
   applyPlugins(app)
   addRoutes(app)
--- src/routes.tsx
+++ src/routes.tsx
@@ -1,7 +1,9 @@
 import Elysia from "elysia";
 import { addTasksRoutes } from "./resources/tasks/routes";
+import { addAuthRoutes } from "./resources/users/routes";
 
 export default function addRoutes(app: Elysia) {
+  addAuthRoutes(app)
   addTasksRoutes(app)
 }
 
